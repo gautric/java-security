f011560b8b2d654c78f3075336aae15d4e2fed2e
gautric <gregoire@autric.net>
2015-03-17 15:54:04 +0100

    first commit

---
+++ security/src/main/java/net/a/g/crypto/security/ProviderListing.java
@@ -0,0 +1,32 @@
+package net.a.g.crypto.security;
+
+import java.security.Provider;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class ProviderListing {
+
+	public static Logger LOG = Logger.getLogger(ProviderListing.class
+			.getCanonicalName());
+
+	public static void main(String[] args) {
+
+		for (Provider provider : Security.getProviders()) {
+			LOG.info(">> "+provider.getName());
+			for (String key : asSortedList(provider.stringPropertyNames())) {
+				LOG.info(" \t\t\t" + key + "\t\t" + provider.getProperty(key));
+			}
+		}
+	}
+
+	public static <T extends Comparable<? super T>> List<T> asSortedList(
+			Collection<T> c) {
+		List<T> list = new ArrayList<T>(c);
+		Collections.sort(list);
+		return list;
+	}
+}
